77d41ee663ed4834c066010dce7506663473b901
Leonardo Certorio <leonardo.certorio@gmail.com>
2023-04-16 20:39:07 -0300

    att

--- ListaRevisão/3/3c/3c.c
+++ ListaRevisão/3/3c/3c.c
@@ -6,33 +6,38 @@
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	
-	int x, y, aux, matriz[TAM][TAM];
-
 	//x=linha ; y=columa
+	int x, y, aux, matriz[TAM][TAM];
 	
-	printf("Digite %d nÃºmeros:\n", TAM);	
+	printf("Preencha uma matriz %d x %d com números:\n", TAM, TAM);
+	
+	//leitura dos números da matriz	
 	for (x = 0; x < TAM; x++) {
 		for (y = 0; y < TAM; y++) {
   			scanf("%d", &matriz[x][y]);
   		}
 	}
+	
+	//definindo variável do maior valor
+	int maiorNumero = matriz[0][0];
 
-	int maior = matriz[0][0];
-
+	//percorrendo as linhas da matriz
 	for (x = 0; x < TAM; x++) {
 		
-		for (y = 0; y < TAM; y++) {			
-			if (matriz[x][y] > maior ) {				
-				maior = matriz[x][y];			
+		//percorrendo as colunas da matriz
+		for (y = 0; y < TAM; y++) {
+			
+			//verificando os números maiores e adicionando na variável "maiorNumero"		
+			if (matriz[x][y] > maiorNumero ) {				
+				maiorNumero = matriz[x][y];			
 			} else {
 				continue;
 			}		
 		}
 	}
 	
-	printf("\n\n");
-	
-	printf("O maior nÃºmero Ã©: ", maior);
+	printf("\n\n");	
+	printf("O maior número é: %d", maiorNumero);
 			
 	return 0;
 	
